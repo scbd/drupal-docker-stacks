8adb5dc5124b0a3b7bb2f0939933a2e2f58c05ef
Randy Houlahan <randy.houlahan@cbd.int>
2017-12-19 16:13:13 -0500

    update secrets

--- local/secrets.sh
+++ local/secrets.sh
@@ -6,4 +6,4 @@ docker secret rm SRCKEY
 docker secret create --label env=local SRCKEY $HOME/.secrets/SRCKEY
 docker secret create --label env=local AURORA_USER $HOME/.secrets/AURORA_USER
 docker secret create --label env=local AURORA_PASS $HOME/.secrets/AURORA_PASS
-docker secret create --label env=local settings.php $HOME/.secrets/local.settings.php
+docker secret create --label env=local settings.php $HOME/.secrets/settings.php

8ee25570d4b11515bffcb3dfbcbb304c0c097f8a
Randy Houlahan <randy.houlahan@cbd.int>
2017-12-19 13:52:00 -0500

    remove volume purge

--- local/drupal/stop.sh
+++ local/drupal/stop.sh
@@ -4,6 +4,5 @@ docker stop docker-sync-drupal-config
 docker stop docker-sync-drupal
 sleep 8s
 printf 'y\n' | docker image prune
-printf 'y\n' | docker volume prune
 docker volume rm docker-sync-drupal
 docker volume rm docker-sync-drupal-config
